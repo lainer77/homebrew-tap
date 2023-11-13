97348bf6535c0238d1168b87d68b24ff76873b06
sunaliner <dh.kim@hanwha-nxef.com>
2023-11-13 15:15:10 +0900

    fix: 해시값 바뀐 오류 수정

--- Formula/react-gpt-cli.rb
+++ Formula/react-gpt-cli.rb
@@ -5,10 +5,10 @@ class ReactGptCli < Formula
   
     if OS.mac?
       url "https://cdn.readyfront.co.kr/pkg/react-gpt-cli-macos"
-      sha256 "2a7dfd4bba721d82776ec666ca993b9704cc3e9198daa1c8975028557abb53e5"
+      sha256 "de7e1e5d66aa055d3b4cfece7bf1d9fa242ff8b373a9249edd2c19b2e3276bfe"
     elsif OS.linux?
       url "https://cdn.readyfront.co.kr/pkg/react-gpt-cli-linux"
-      sha256 "de7e1e5d66aa055d3b4cfece7bf1d9fa242ff8b373a9249edd2c19b2e3276bfe"
+      sha256 "2a7dfd4bba721d82776ec666ca993b9704cc3e9198daa1c8975028557abb53e5"
     elsif OS.windows?
       url "https://cdn.readyfront.co.kr/pkg/react-gpt-cli-win.exe"
       sha256 "70fe9e4d8a5e1efbd1b92ef60072db371578b19104d38e769f4e38cdf2c35252"
